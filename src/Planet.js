1555a7467a71d0196869e1632d8e7cb569cccf09
jordanallain <jordan.allain@generalassemb.ly>
2018-11-30 16:51:52 -0500

    Add planet component with css

---
+++ src/Planet.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Planet.css'
+
+class Planet extends Component {
+  handleClick = () => {
+
+    this.setState(prevState => {return {
+      visited: !this.state.visited
+    }})
+  }
+  state = {
+    visited: false
+  }
+  render() {
+    return (
+      <div className='planet'>
+        <h2>{this.props.name}</h2>
+        <h3>{this.props.climate}</h3>
+        <button onClick={this.handleClick}>{this.state.visited ? "Whoops didn't visit that" : "Visited?"}</button>
+      </div>
+
+    )
+  }
+}
+
+export default Planet;
