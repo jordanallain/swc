656543f622eda6799883655dd129f647d95ee969
jordanallain <jordan.allain@generalassemb.ly>
2019-01-08 15:07:47 -0500

    Make landing page welcome component

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Welcome</h1>
+        <p>Click a link to find out more!</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Welcome;
