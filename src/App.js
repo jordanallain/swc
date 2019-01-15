074a070c5345c23b3c327dc3856a9cd53382dcf1
jordanallain <jordan.allain@generalassemb.ly>
2019-01-15 11:10:13 -0500

    Move state of 'visited up to App component
    - Move the state of each planet's 'visited' property up into the App
      component so that changing routes wouldn't reset visited.

--- src/App.js
+++ src/App.js
@@ -10,28 +10,37 @@ class App extends Component {
   state = {
     planets: []
   }
+  toggleVisited = (planet) => {
+    const planetArray = this.state.planets.map(plan => plan)
+    const currentPlanet = planetArray.find(plan => plan.name === planet.props.name)
+    currentPlanet.visited = !currentPlanet.visited
+    const index = planetArray.indexOf(planet)
+    planetArray[index] = currentPlanet
+    this.setState({planets: planetArray})
+  }
   componentDidMount() {
     axios('https://swapi.co/api/planets')
       .then(resp => {
-        console.log(resp.data.results)
-        let planetArray = resp.data.results.map(planet => (<Planet
-        key={planet.name}
-        climate={planet.climate}
-        name={planet.name}
-        />)
+        let planetArray = resp.data.results.map(planet => ({climate: planet.climate, name:planet.name, visited: false})
       )
       this.setState({planets: planetArray})
     })
   }
   render() {
-    console.log(this.state.planets)
     return (
       <div className="App">
         <Nav className="nav"/>
         <Route exact path='/' component={Welcome} />
         <Route path='/planets' render={() => (
           <div className="planet-container">
-            {this.state.planets[0] ? this.state.planets : '...'}
+            {this.state.planets[0] ? this.state.planets.map((planet) => (
+              <Planet
+                toggleVisited={this.toggleVisited}
+                key={planet.name}
+                name={planet.name}
+                climate={planet.climate}
+                visited={planet.visited}/>
+            )) : 'loading...'}
           </div>
           )} />
       </div>
--- src/Planet.js
+++ src/Planet.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react'
 import './Planet.css'
 
 class Planet extends Component {
-  handleClick = () => {
-
-    this.setState(prevState => {return {
-      visited: !this.state.visited
-    }})
-  }
-  state = {
-    visited: false
-  }
   render() {
     return (
       <div className='planet'>
         <h2 className="planet-name">{this.props.name}</h2>
         <h3>{this.props.climate}</h3>
-        <button onClick={this.handleClick}>{this.state.visited ? "Whoops didn't visit that" : "Visited?"}</button>
+        <button onClick={() => {this.props.toggleVisited(this)}}>{this.props.visited ? "Whoops didn't visit that" : "Visited?"}</button>
       </div>
 
     )
