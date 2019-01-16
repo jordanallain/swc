8f8c39e99a5db8bd62caf7dadc9533740aecfdd0
jordanallain <jordan.allain@generalassemb.ly>
2019-01-16 10:32:30 -0500

    Style Planet component to look more planet-like

--- src/Planet.js
+++ src/Planet.js
@@ -3,9 +3,11 @@ import './Planet.css'
 
 class Planet extends Component {
   render() {
+    const climateClass = this.props.climate.includes('arid') ? 'arid' : this.props.climate.includes('murky') ? 'murky' : this.props.climate.includes('frozen') ? 'frozen' :
+    this.props.climate.includes('tropical') ? 'tropical' : 'temperate'
     return (
-      <div className='planet'>
-        <h2 className="planet-name">{this.props.name}</h2>
+      <div className={climateClass + " planet"}>
+        <h2 className={"planet-name " + (this.props.visited ? 'visited' : '')}>{this.props.name}</h2>
         <h3>{this.props.climate}</h3>
         <button onClick={() => {this.props.toggleVisited(this)}}>{this.props.visited ? "Whoops didn't visit that" : "Visited?"}</button>
       </div>
