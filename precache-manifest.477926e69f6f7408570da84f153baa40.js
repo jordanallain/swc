8222eee1c6a400dc2dc2af208512fdc1b2c8dc1c
jordanallain <jordan.allain@generalassemb.ly>
2019-01-31 14:10:56 -0500

    Updates

---
+++ precache-manifest.477926e69f6f7408570da84f153baa40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a27bb5419fa62ecc00697bf071243e6",
+    "url": "/static/media/space.0a27bb54.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21a309b5320faacb3766",
+    "url": "/static/js/main.21a309b5.chunk.js"
+  },
+  {
+    "revision": "5391eec79384bd025804",
+    "url": "/static/js/1.5391eec7.chunk.js"
+  },
+  {
+    "revision": "21a309b5320faacb3766",
+    "url": "/static/css/main.92499ea8.chunk.css"
+  },
+  {
+    "revision": "78384a8f8252f385456bde854b8102d0",
+    "url": "/index.html"
+  }
+];
